4e5328ea00a7f8c45cc2ce2d54ff13df5f4f5542
DaemonNikolay <nikolay-eckert@mail.ru>
2018-07-15 16:01:17 +0700

    Create tools for upload video to DB

--- PythonV2/examples.py
+++ PythonV2/examples.py
@@ -5,47 +5,44 @@ from local_data import db
 
 
 def main():
+    # path = 'local_data/video.json'
+    # file_video = open(path, 'r', encoding='utf8')
+    # content = json.loads(file_video.read())
+    # file_video.close()
+    #
+    # for element in content['video']:
+    #     print(element)
 
-    path = 'local_data/videos.json'
-    file_video = open(path, 'r', encoding='utf8')
-    content = json.loads(file_video.read())
-    file_video.close()
 
-    print(content['video'][1])
-    
-    
-    
-    # """ Пример загрузки фото """
-    # 
-    # login, password = '89618754106', 'ubgcfy22331'
-    # vk_session = vk_api.VkApi(login, password, scope='wall, messages, photos, video')
-    # 
-    # try:
-    #     vk_session.auth(token_only=True)
-    # except vk_api.AuthError as error_msg:
-    #     print(error_msg)
-    #     return
-    # 
-    # vk = vk_session.get_api()
-    # 
-    # response = vk.video.save(description='This is text',
-    #                          # отсутствие текста или символ пробела говорит о том, что нужно использовать текст по default из видео
-    #                          link='https://www.youtube.com/watch?v=RK1K2bCg4J8',
-    #                          group_id=db.GroupTest.group_id,
-    #                          album_id=db.GroupTest.album_id_video)
-    # 
-    # attachment = '{0}{1}_{2}'.format('video', response['owner_id'], response['video_id'])
-    # 
-    # query = requests.get(url=response['upload_url'])
-    # print(query.text)
-    # query.close()
-    # 
-    # vk.wall.post(owner_id=-db.GroupTest.owner_id,
-    #              from_group=db.GroupTest.from_group,
-    #              message='Test video 1',
-    #              attachments=attachment)
-    # 
-    # print(response)
+    login, password = '89618754106', 'ubgcfy22331'
+    vk_session = vk_api.VkApi(login, password, scope='wall, messages, photos, video')
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    vk = vk_session.get_api()
+
+    response = vk.video.save(description='This is text',
+                             # отсутствие текста или символ пробела говорит о том, что нужно использовать текст по default из видео
+                             link='https://www.youtube.com/watch?v=RK1K2bCg4J8',
+                             group_id=db.GroupTest.group_id,
+                             album_id=db.GroupTest.album_id_video)
+
+    attachment = '{0}{1}_{2}'.format('video', response['owner_id'], response['video_id'])
+
+    query = requests.get(url=response['upload_url'])
+    print(query.text)
+    query.close()
+
+    vk.wall.post(owner_id=-db.GroupTest.owner_id,
+                 from_group=db.GroupTest.from_group,
+                 message='Test video 1',
+                 attachments=attachment)
+
+    print(response)
 
 
 if __name__ == '__main__':
---
+++ PythonV2/tools/upload_video.py
@@ -0,0 +1,84 @@
+import vk_api
+import pymysql
+import os
+import json
+from local_data import db
+
+
+def auth():
+    login, password = db.PrivateDataVk.login, db.PrivateDataVk.password
+    vk_session = vk_api.VkApi(login, password, scope=db.PrivateDataVk.scope)
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        return 'Auth crash: ' + str(error_msg)
+
+    return vk_session
+
+
+def insert_video(message, attachments, tag):
+    try:
+        connection = pymysql.connect(host=db.Database.host,
+                                     user=db.Database.username,
+                                     db=db.Database.name_db,
+                                     password=db.Database.password,
+                                     charset=db.Database.charset)
+
+        with connection.cursor() as cursor:
+            sql = """INSERT INTO video (message, attachments, tag) 
+                            VALUES ('{0}','{1}','{2}')""".format(message, attachments, tag)
+            cursor.execute(sql)
+            connection.commit()
+
+        print('Video  "{0}" - successfully!'.format(message))
+
+        return cursor.fetchone()
+
+    except Exception as e:
+        print('Error INSERT video: Exception: {0}'.format(e))
+
+    finally:
+        connection.close()
+
+
+def main():
+    vk = auth()
+    if (str(vk).startswith('Auth crash')):
+        print(vk)
+        return
+
+    path = '../local_data/video.json'
+
+    if not os.path.isfile(path):
+        print('File "{0}" not found!'.format(path))
+        return
+
+    file_posts = open(path, 'r', encoding='utf8')
+    content = json.loads(file_posts.read())
+    file_posts.close()
+
+    vk_use_api = vk.get_api()
+
+    for element in content['video']:
+        attachments = ''
+        tag = element['tag_id']
+
+        for video_link in element['links']:
+            response = vk_use_api.video.save(description=tag,
+                                             link=video_link,
+                                             group_id=db.GroupTest.group_id,
+                                             album_id=db.GroupTest.album_id_video)
+
+            attachments += '{0}{1}_{2},'.format('video', response['owner_id'], response['video_id'])
+
+        message = element['message']
+        attachments = attachments[0:len(attachments) - 1]
+
+        insert_video(message=message,
+                     attachments=attachments,
+                     tag=tag)
+
+
+if __name__ == '__main__':
+    main()
