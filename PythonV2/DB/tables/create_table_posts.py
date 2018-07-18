41f777572278ce63bdc407a98c2dbb71f6870e15
DaemonNikolay <nikolay-eckert@mail.ru>
2018-07-18 20:56:47 +0700

    Fic create DB; add data description for posts

--- PythonV2/DB/tables/create_table_posts.py
+++ PythonV2/DB/tables/create_table_posts.py
@@ -3,7 +3,7 @@ def create_table_posts(connection):
         with connection.cursor() as cursor:
             sql = """CREATE TABLE posts(
                         id INT AUTO_INCREMENT PRIMARY KEY,
-                        content VARCHAR(500) NOT NULL,
+                        content TEXT NOT NULL,
                         attachments VARCHAR(200),
                         url VARCHAR(255),
                         tag INT NOT NULL,
